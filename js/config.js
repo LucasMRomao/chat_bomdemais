bb95beb87540c136875a1e336345cb618f3e5a27
Lucas Martins Romão <lmartins.romao@gmail.com>
2026-01-10 19:05:48 -0300

    Iniciada construção da tela de configurações

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    URL_SERVIDOR_SOCKET: "http://localhost:3000",
+    URL_API: "http://localhost:3333"
+}
--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,22 @@
+function abrirTelaConfiguracoes(usuario, senha){
+    $.ajax({
+        url: CONFIG.URL_API + "/getUsuarioByCredentials",
+        method: "GET",
+        data: {
+            usuario: usuario,
+            senha: senha
+        },
+        success: (result) => {
+            if(result[0][0]){
+                alert("Usuário: " + result[0][0].usuario + " / Senha: " + result[0][0].senha)
+            }else{
+                $("#modal-configuracoes").modal('hide')
+                $("#modal-credenciais-incorretas").modal('show');
+            }
+        }
+    });
+}
+
 $(() => {
     $("#bEntrar").click(() => {
         let usuario = $("#iUsuario").val();
@@ -14,7 +33,23 @@ $(() => {
         }
     });
 
-    var socket = io("http://localhost:3000");
+    $("#bLogarConfiguracoes").click(() => {
+        let usuario = $("#iUsuarioConfiguracoes").val();
+        let senha = $("#iSenhaConfiguracoes").val();
+
+        if(!usuario || !senha){
+            alert("Ambos os campos devem ser preenchidos!");
+        }else{
+            abrirTelaConfiguracoes(usuario, senha);
+        }
+    }); 
+
+    $("#btn-credenciais-incorretas").click(() => {
+        $("#modal-credenciais-incorretas").modal('hide');
+        $("#modal-configuracoes").modal('show');
+    });
+
+    var socket = io(CONFIG.URL_SERVIDOR_SOCKET);
 
     socket.emit("teste", 'Bom', 'Demais');
 
