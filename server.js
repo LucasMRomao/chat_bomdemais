85be5ae4dea87a0c95fec22c44aded509a84ddc0
Lucas Martins Romão <lmartins.romao@gmail.com>
2026-01-24 18:20:39 -0300

    Implementado funcionalidade para não permitir um mesmo usuário logar 2 vezes

--- server.js
+++ server.js
@@ -6,14 +6,45 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+var usuariosOnline = new Array();
+
 io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
+
+    socket.on("disconnect", () => {
+        let index = usuariosOnline.findIndex((user) => user.socketid == socket.id);
+        usuariosOnline.splice(index, 1);
+        io.emit("atualiza-usuarios-online", usuariosOnline);
+    })
 
     socket.on("teste", (a, b) => {
         console.log(`Test emmitted: ${a} + ${b}`);
         socket.emit("retorno_teste", "Funcionou!", "Que orgulho!", "Bom demais")
     });
-})
+
+    socket.on("user_online", (nome, username) => {
+        let usuario = {
+            nome: nome,
+            usuario: username,
+            socketid: socket.id
+        };
+
+        usuariosOnline.push(usuario);
+        io.emit("atualiza-usuarios-online", usuariosOnline);
+    });
+
+    socket.on("user_offline", (username) => {
+        let index = usuariosOnline.findIndex((user) => user.usuario == username);
+        usuariosOnline.splice(index, 1); //O primeiro parâmetro é o índice a iniciar a remoção. O segundo é a quantidade de itens a remover
+        io.emit("atualiza-usuarios-online", usuariosOnline);
+    });
+
+    socket.on("verifica-usuario-online", (username) => {
+        console.log(`Verificando se ${username} está online`);
+        let index = usuariosOnline.findIndex((user) => user.usuario == username);
+        socket.emit("retorno-verifica-online", index > -1);
+    });
+});
 
 server.listen(3000, () => {
     console.log(`Server running at http://localhost:3000`);
