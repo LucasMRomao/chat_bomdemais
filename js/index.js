62a2570dd7d89a9ba6e9e246db93ffdfdb41b828
Lucas Martins Romão <lmartins.romao@gmail.com>
2026-01-25 11:28:53 -0300

    Salvando para continuar no PC

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const atualizarUsuariosOnline = (lista) => {
 
     for(var i in lista){
         console.log(lista[i].id);
-        let $user = `<a href='#' class='list-group-item list-group-item-action' userid='${lista[i].id}' username='${lista[i].usuario}'>${lista[i].nome}</a>`;
+        let $user = `<a href='#' class='list-group-item list-group-item-action' userid='${lista[i].id}' username='${lista[i].usuario}'>${lista[i].nome}</a>`; //<span class="badge text-bg-warning">!</span>
         $("#ulContatos").append($user);
     }
 
@@ -18,14 +18,22 @@ const atualizarUsuariosOnline = (lista) => {
         $(event.currentTarget).addClass("active");
         $("#iMensagemEnviar").prop("disabled", false);
         $("#bEnviarMensagem").prop("disabled", false);
+        
+        /*let listaAlertas = $(".span-alerta-mensagem");
+        console.log(listaAlertas);
+        console.log(listaAlertas.parent().attr("userid"));
+
+        for(let i in listaAlertas){
+            if(listaAlertas[i].parent().attr("userid") == $(event.currentTarget).attr("userid")) $(listaAlertas[i]).remove();
+        }*/
     });
 }
 
 const sinalizarUsuarioOnline = async () => {
     let dados = await pegarDadosUsuarioOnline();
-    socket.emit("user-online", dados.id, dados.nome, dados.usuario);
     usuarioLogado = dados.usuario;
     idUsuarioLogado = dados.id;
+    socket.emit("user-online", dados.id, dados.nome, dados.usuario);
 }
 
 $(() => {
@@ -46,13 +54,25 @@ $(() => {
         atualizarUsuariosOnline(usuariosOnline);
     });
 
+    socket.on("receber-mensagem", (idUsuarioEnvia, mensagem) => {
+        console.log(`ID Usuário: ${idUsuarioEnvia} - MENSAGEM: ${mensagem}`);
+
+        let userAtivo = $("#ulContatos>.active");
+        
+        if($(userAtivo).attr("userid") == idUsuarioEnvia){
+            //O usuário ativo é quem enviou a mensagem
+        }else{
+            $(`#ulContatos>[userid=${idUsuarioEnvia}]`).append(`<span class="badge text-bg-warning span-alerta-mensagem">!</span>`);
+        }
+    });
+
     $("#bEnviarMensagem").click(() => {
         let usuarioID = $("#ulContatos>.active").attr("userid");
         let mensagem = $("#iMensagemEnviar").val();
 
-        console.log(usuarioID);
+        /*console.log(usuarioID);
         console.log(idUsuarioLogado);
-        console.log(mensagem);
+        console.log(mensagem);*/
 
         $.ajax({
             url: CONFIG.URL_API + "/mensagens",
@@ -65,6 +85,7 @@ $(() => {
             success: (result) => {
                 console.log(result);
                 $("#iMensagemEnviar").val("");
+                socket.emit("enviar-mensagem", idUsuarioLogado, usuarioID, mensagem);
             }
         });
     });
